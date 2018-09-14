0ac901bc03a8c2d7a14a802729f6622db98ecade
Joe <joe.guzikowski@gmail.com>
2018-09-13 22:25:06 -0400

    added onload feature so it's invisible when it's half loaded

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Logo.css';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
 
-const logo = (props) =>(
-    <div className={classes.Logo}>
-        <img src={burgerLogo} alt="MyBurger" />
-    </div>
-);
+class logo extends Component {
+    state = {
+        loading: true
+    }
+
+    handleImageLoaded = () => {
+        console.log("finished loading");
+        this.setState({loading: false});
+    }
+
+    render () {
+        return (
+            <div className={classes.Logo} 
+                    style={{
+                        transform: this.props.loading ? 'translateX(-100vh)' : 'translateX(0)',
+                        visibility: this.state.loading ? 'hidden' : 'visible'
+                    }} >
+                <img 
+                    onLoad={this.handleImageLoaded}
+                    src={burgerLogo} alt="MyBurger" 
+                    />
+            </div>
+        );
+    }
+}
 
 export default logo;
